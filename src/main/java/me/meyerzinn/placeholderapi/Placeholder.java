9b60961b9823a0a29a9d94db75e8708cdfc41954
20zinnm <meyerzinn@gmail.com>
2016-03-06 23:42:11 -0600

    For real this time.

--- src/main/java/me/meyerzinn/placeholderapi/Placeholder.java
+++ src/main/java/me/meyerzinn/placeholderapi/Placeholder.java
@@ -2,11 +2,13 @@ package me.meyerzinn.placeholderapi;
 
 import org.spongepowered.api.entity.living.player.User;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by meyerzinn on 3/6/16.
  */
 public interface Placeholder {
 
-    String replace(User user, String placeholder);
+    String replace(User user, Pattern pattern, String placeholder);
 
 }
